b72310f5022f4aa5756299bac9481f1551b4315d
ljw0829 <olo_jinwon@naver.com>
2022-06-09 22:37:08 +0900

    - feat : Exception Handler timestamp 추가

--- src/main/java/com/jinwon/ssoauth/web/dto/ErrorDto.java
+++ src/main/java/com/jinwon/ssoauth/web/dto/ErrorDto.java
@@ -5,19 +5,25 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
+
 @Getter
 @ApiModel(value = "예외 핸들러 객체")
 public class ErrorDto {
 
+    @ApiModelProperty(value = "발생 시간")
+    private final LocalDateTime timestamp;
+
     @ApiModelProperty(value = "상태")
     private final HttpStatus status;
 
-    @ApiModelProperty(value = "메시지 코드")
-    private final String messageCode;
+    @ApiModelProperty(value = "에러 코드")
+    private final String error;
 
-    public ErrorDto(HttpStatus status, String messageCode) {
+    public ErrorDto(HttpStatus status, String error) {
         this.status = status;
-        this.messageCode = messageCode;
+        this.error = error;
+        this.timestamp = LocalDateTime.now();
     }
 
 }
