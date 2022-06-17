0b96803f7c4cc1d13647b45c8e9ffc637f5d3255
이진원 <ljw0829@midasit.com>
2022-06-17 21:30:44 +0900

    - feat : redis-config 설정 변경

--- src/main/java/com/jinwon/ssoauth/infra/config/RedisConfig.java
+++ src/main/java/com/jinwon/ssoauth/infra/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.jinwon.ssoauth.infra.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
@@ -7,35 +8,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 /**
  * Redis 설정
  */
 @RefreshScope
 @Configuration
-@EnableRedisHttpSession
-public class RedisConfig extends AbstractHttpSessionApplicationInitializer {
+@RequiredArgsConstructor
+public class RedisConfig {
 
-    /* 주입 옵션으로 변경 예정 */
-    @Value("${redis.host}")
+    @Value("${spring.redis.host}")
     private String host;
 
-    @Value("${redis.port}")
+    @Value("${spring.redis.port}")
     private int port;
 
-    @Bean
-    public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
+    private final LettuceConnectionFactory connectionFactory;
 
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(connectionFactory());
+        redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
     }
 
